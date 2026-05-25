137158327313b5d45c5da5d1435bd4866ae11a3b
Simon Völkl <s.voelkl2@oth-aw.de>
2026-05-25 23:16:23 +0200

    Update AWS CDK stack configuration and adjust example resource imports for clarity

--- sys-src/sensiq-aws/lib/sensiq-cdk-stack.ts
+++ sys-src/sensiq-aws/lib/sensiq-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+// example resource import
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class SensiqCdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -9,9 +11,9 @@ export class SensiqCdkStack extends cdk.Stack {
         // The code that defines your stack goes here
 
         // example resource
-        // const queue = new sqs.Queue(this, 'AwsInitTestQueue', {
-        //   visibilityTimeout: cdk.Duration.seconds(300)
-        // });
+        const queue = new sqs.Queue(this, 'SensiqTestQueue', {
+            visibilityTimeout: cdk.Duration.seconds(300)
+        });
 
     }
 }
