7224ad68f6bb30d43c0a97ebe0663439e1bdff1f
Simon Völkl <s.voelkl2@oth-aw.de>
2026-05-25 23:50:53 +0200

    Refactor SensiqCdkStack: Add demo comments for Lambda function and CloudFormation output resources

--- sys-src/sensiq-aws/lib/sensiq-cdk-stack.ts
+++ sys-src/sensiq-aws/lib/sensiq-cdk-stack.ts
@@ -4,16 +4,44 @@ import { Construct } from 'constructs';
 // example resource import
 import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+// Import the Lambda module
+// import * as lambda from 'aws-cdk-lib/aws-lambda';
+
 export class SensiqCdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
-
-        // The code that defines your stack goes here
 
         // example resource
         const queue = new sqs.Queue(this, 'SensiqTestQueue', {
             visibilityTimeout: cdk.Duration.seconds(300)
         });
+
+        // Define the Lambda function resource.
+        // arguments: scope, id, properties (with runtime, handler, code)
+        // const historyHellowWorldLambda = new lambda.Function(this, "HistoryHelloWorldLambda", {
+        //     runtime: lambda.Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+        //     handler: "index.handler",
+        //     code: lambda.Code.fromInline(`
+        //         exports.handler = async function(event) {
+        //             return {
+        //                 statusCode: 200,
+        //                 body: JSON.stringify('Hello CDK!'),
+        //             };
+        //         };
+        //     `),
+        // });
+
+        // Define the Lambda function URL resource.
+        // Do not use Auth Type NONE, as it would be publicly accessible without authentication.
+        // const historyHelloWorldLambdaUrl = historyHellowWorldLambda.addFunctionUrl({
+        //     authType: lambda.FunctionUrlAuthType.NONE,
+        // });
+
+        // Define a CloudFormation output for your URL
+        // new cdk.CfnOutput(this, "historyHelloWorldLambdaUrlOutput", {
+        //     value: historyHelloWorldLambdaUrl.url,
+        // })
+
 
     }
 }
