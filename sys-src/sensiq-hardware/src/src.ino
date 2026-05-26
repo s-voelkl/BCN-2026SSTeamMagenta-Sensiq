6f757ec484017189ac5a2dad0b5a9c674c6cd0a8
Simon Völkl <s.voelkl2@oth-aw.de>
2026-05-26 11:35:28 +0200

    Move synthTime method to TimeHelper, add Unit Test

--- sys-src/sensiq-hardware/src/src.ino
+++ sys-src/sensiq-hardware/src/src.ino
@@ -1,37 +1,11 @@
 #include "WifiConnection.h"
 #include "MqttConnection.h"
 #include "SensorHelper.h"
+#include "TimeHelper.h"
 #include <AUnit.h>
 
 // Set to 1 to run AUnit tests; set 0 for main functionality.
 #define RUN_TESTS 0
-
-/**
- * @brief Synchronizes the MCU's internal clock with an NTP server.
- * This is crucial for accurate timestamps in sensor data and for validating TLS certificates during MQTT communication.
- *
- * @note The function will block until the time is successfully synchronized, which may take a few seconds depending on network conditions.
- * It uses the configTime function to set up NTP synchronization and waits until a valid time is obtained before proceeding.
-
- * @warning A stable Wifi connection is required.
- */  
-void syncTime() {
-  Serial.print("Synchronizing time...");
-
-  // configures the Network Time Protocol (NTP) client with Greenwich Mean Time (GMT) offset of 0 seconds (UTC).
-  // Primary and secondary NTP servers are specified for redundancy. 
-  configTime(0, 0, "pool.ntp.org", "time.nist.gov");
-  
-  // seconds since unix epoch (Jan 1, 1970)
-  time_t now = time(nullptr);
-  // Wait until the time is greater than Jan 1, 2020 (1577836800 seconds since epoch)
-  while (now < 1577836800) {
-      delay(500);
-      Serial.print(".");
-      now = time(nullptr);
-  }
-  Serial.println("\nTime synchronized.");
-}
 
 /**
  * @brief Main setup function for the MCU controller.
