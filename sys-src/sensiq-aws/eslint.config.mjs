a7b9b374dfc3d126e577e81a8500386bab6524aa
Johannes Schieder <146852991+JSchOBL@users.noreply.github.com>
2026-05-28 20:44:43 +0200

    Ignore coverage in eslint

--- sys-src/sensiq-aws/eslint.config.mjs
+++ sys-src/sensiq-aws/eslint.config.mjs
@@ -1,6 +1,6 @@
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ["node_modules/**", "cdk.out/**"] },
+  { ignores: ["node_modules/**", "cdk.out/**", "coverage/**"] },
   ...tseslint.configs.recommended
 );
