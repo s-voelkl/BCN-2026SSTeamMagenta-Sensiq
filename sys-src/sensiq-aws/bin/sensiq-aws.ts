4340ed90ed81349c40cf588cfeeb1e446a2dff4d
Simon Völkl <s.voelkl2@oth-aw.de>
2026-05-29 12:34:09 +0200

    Remove SensiqExampleStack and related test files to streamline the project structure

--- sys-src/sensiq-aws/bin/sensiq-aws.ts
+++ sys-src/sensiq-aws/bin/sensiq-aws.ts
@@ -2,7 +2,6 @@
 import * as cdk from 'aws-cdk-lib/core';
 import { SensiqIotDeviceStack } from '../lib/sensiq-iot-device-stack';
 import { IotCoreStack } from '../lib/sensiq-iot-stack';
-import { SensiqExampleStack } from '../lib/sensiq-example-stack';
 import { SensiqAthenaStack } from '../lib/sensiq-athena-stack';
 
 const app = new cdk.App();
@@ -11,8 +10,6 @@ const deviceStack = new SensiqIotDeviceStack(app, 'SensiqIotDeviceStack');
 
 const iotCoreStack = new IotCoreStack(app, 'SensiqIotCoreStack');
 
-const exampleStack = new SensiqExampleStack(app, 'SensiqExampleStack');
-
-const athenaStack = new SensiqAthenaStack(app, 'SensiqAthenaStack');
+const athenaStack = new SensiqAthenaStack(app, 'SensiqAthenaStack'); // eslint-disable-line
 
 iotCoreStack.addDependency(deviceStack);
