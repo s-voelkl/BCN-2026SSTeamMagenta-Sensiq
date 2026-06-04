ab7544342ac4712ad9f0a386365d797e27f1c562
Andre Tien Vu <a.vu@oth-aw.de>
2026-06-04 16:57:10 +0200

    Merge main into feat/api-gateway

--- sys-src/sensiq-aws/bin/sensiq-aws.ts
+++ sys-src/sensiq-aws/bin/sensiq-aws.ts
@@ -1,14 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { SensiqIotDeviceStack } from '../infra/sensiq-iot-device-stack';
-<<<<<<< HEAD
 import { SensiqLiveStack } from '../infra/sensiq-live-stack';
 import { SensiqHistoryStack } from '../infra/sensiq-history-stack';
 import { SensiqApiStack } from '../infra/sensiq-api-stack';
-=======
-import { SensiqLiveStack } from '../infra/sensiq-live-stack';
-import { SensiqHistoryStack } from '../infra/sensiq-history-stack';
->>>>>>> origin/main
 
 const app = new cdk.App();
 
@@ -18,12 +13,8 @@ const historyStack = new SensiqHistoryStack(app, 'SensiqHistoryStack'); // eslin
 
 const liveStack = new SensiqLiveStack(app, 'SensiqLiveStack');
 
-<<<<<<< HEAD
 const apiStack = new SensiqApiStack(app, 'SensiqApiStack');
 
 liveStack.addDependency(deviceStack);
 apiStack.addDependency(liveStack);
 apiStack.addDependency(historyStack);
-=======
-liveStack.addDependency(deviceStack);
->>>>>>> origin/main
