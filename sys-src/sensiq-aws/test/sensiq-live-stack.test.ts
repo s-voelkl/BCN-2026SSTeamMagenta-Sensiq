ee2a983732089e91c1f98da7788e4a9b4748bd8b
Sebastian.Rosner <bdb3@oth-aw.de>
2026-06-07 17:11:17 +0200

    Create sensiq-live-stack.test.ts
    Adding tests to the live infrastructure

---
+++ sys-src/sensiq-aws/test/sensiq-live-stack.test.ts
@@ -0,0 +1,73 @@
+import * as cdk from 'aws-cdk-lib';
+import { Template, Match } from 'aws-cdk-lib/assertions';
+import { SensiqLiveStack } from '../infra/sensiq-live-stack';
+
+let template: Template;
+
+beforeAll(() => {
+    const app = new cdk.App();
+    const stack = new SensiqLiveStack(app, 'TestLiveStack');
+    template = Template.fromStack(stack);
+});
+
+test('Table created corectly', () => {
+    template.hasResourceProperties('AWS::DynamoDB::Table', {
+        TableName: 'SensiqLiveState',
+        ProvisionedThroughput: {
+            ReadCapacityUnits: 1,
+            WriteCapacityUnits: 1
+        },
+        TimeToLiveSpecification: {
+            AttributeName: 'expiresAt',
+            Enabled: true
+        },
+        KeySchema: [
+            {
+                AttributeName: 'device_id',
+                KeyType: 'HASH'
+            }
+        ]
+    });
+});
+
+test('Validation Lambda created corectly', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Handler: 'handle_validation.handler',
+        Runtime: Match.stringLikeRegexp('python3.*'),
+        Timeout: 15,
+        Environment: {
+            Variables: Match.objectLike({
+                TABLE_NAME: Match.anyValue()
+            })
+        }
+    });
+});
+
+test('Live Data Lambda Function created corectly', () => {
+    template.hasResourceProperties('AWS::Lambda::Function', {
+        Handler: 'handle_live_data.handler',
+        Runtime: Match.stringLikeRegexp('python3.*'),
+        Timeout: 15,
+        Environment: {
+            Variables: Match.objectLike({
+                TABLE_NAME: Match.anyValue()
+            })
+        }
+    });
+});
+
+
+test('Authorization Check', () => {
+    template.hasResourceProperties('AWS::IAM::Policy', {
+        PolicyDocument: Match.objectLike({
+            Statement: Match.arrayWith([
+                Match.objectLike({
+                    Effect: 'Allow',
+                    Action: Match.arrayWith([
+                        'dynamodb:PutItem'
+                    ])
+                })
+            ])
+        })
+    });
+});
