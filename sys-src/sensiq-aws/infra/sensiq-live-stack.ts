92cc3eab7a61fc96be993ce81a84f9461ed64af2
Simon Völkl <s.voelkl2@oth-aw.de>
2026-06-09 22:17:08 +0200

    Refine comments and identation, set lambda timeout from 15 to 29 seconds
    29 seconds, as API Gateway max limit.

--- sys-src/sensiq-aws/infra/sensiq-live-stack.ts
+++ sys-src/sensiq-aws/infra/sensiq-live-stack.ts
@@ -8,69 +8,73 @@ import { Construct } from 'constructs';
 import path from 'path';
 
 
-
+/**
+ * SensiQLiveStack defines the AWS infrastructure for handling live sensor data from ESP32 devices.
+ * It includes:
+ * - A DynamoDB table ('LiveDataDB') to store incoming sensor data.
+ * - A Python-based Lambda function for validating and processing the sensor data before saving it to the database.
+ * - An IoT Topic Rule that triggers the Lambda function whenever new data is published to the 'sensiq/+/data' topic.
+ * - Another Python-based Lambda function to retrieve live data for a specific device and check if it's offline based on the last timestamp.
+ */
 export class SensiqLiveStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    
-    
-// Creates a DynamoDB table ('LiveDataDB') for ESP32 sensor data.
-// Partition Key: 'device_id' (String) to identify each device.
-// Billing: Provisioned with minimal capacity (1 read / 1 write) 
-// Removal Policy: DESTROY (table and data are deleted on stack teardown)
 
-
-    const liveTable = new dynamodb.Table(this,'LiveDataDB',{
+    // Creates a DynamoDB table for live sensor data
+    // read/write only newest sample, so keep minimal capacity
+    // provisioned billing mode as the traffic is predictable and low, with 1 read and 1 write capacity unit.
+    // destroy on cdk removal as no long term data retention is needed
+    const liveTable = new dynamodb.Table(this, 'LiveDataDB', {
       tableName: 'LiveDataDB',
-      partitionKey:{
-        name:'device_id',
-        type:dynamodb.AttributeType.STRING,
+      partitionKey: {
+        name: 'device_id',
+        type: dynamodb.AttributeType.STRING,
       },
-
       billingMode: dynamodb.BillingMode.PROVISIONED,
       readCapacity: 1,
-      writeCapacity:1,
+      writeCapacity: 1,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
-    } );
+    });
 
     // Creates a Python-based Lambda function for data validation.
-    // Runtime & Timeout: Uses Python 3.12 with a 15-second execution timeout.
-    // The code reads the database name from the environment to dynamically connect and save the sensor data to the correct DynamoDB table.
-
+    // The code reads the database name from the environment to dynamically
+    // connect and save the sensor data to the correct DynamoDB table.
     const lambdaHandleValidation = new PythonFunction(this, 'HandleValidation', {
-        entry: path.join(__dirname, '..', 'src', 'lambda', 'validation'), // points to the directory containing the lambda function code
-        index: 'handle_validation.py', // the file containing the lambda handler
-        handler: 'handler',
-        runtime: lambda.Runtime.PYTHON_3_12,
-        timeout: cdk.Duration.seconds(15),
-        environment: {
+      entry: path.join(__dirname, '..', 'src', 'lambda', 'validation'), // directory
+      index: 'handle_validation.py', // file
+      handler: 'handler',
+      runtime: lambda.Runtime.PYTHON_3_12,
+      timeout: cdk.Duration.seconds(29),
+      environment: {
         TABLE_NAME: liveTable.tableName
-        }}
+      }
+    }
     );
 
     liveTable.grantReadWriteData(lambdaHandleValidation);
 
-    
+    // When new data is published to the 'sensiq/+/data' topic, the IoT Topic Rule triggers
+    // the Lambda function to validate and process the incoming sensor data before saving it to the DynamoDB table.
     new iot.TopicRule(this, 'LiveRule', {
       sql: iot.IotSql.fromStringAsVer20160323("SELECT * FROM 'sensiq/+/data'"),
-      actions: [ new actions.LambdaFunctionAction(lambdaHandleValidation) ],
+      actions: [new actions.LambdaFunctionAction(lambdaHandleValidation)],
     });
 
-      // Function to return live data
-      // Runtime & Timeout: Uses Python 3.12 with a 15-second execution timeout.
-      //This code retrieves the latest data for a specific device from the DynamoDB table and checks if the device is offline by verifying if its last timestamp is older than 6 minutes.
-       const lambdaHandleLiveData = new PythonFunction(this, 'HandleLiveData', {
+    // Function to return live data
+    // This code retrieves the latest data for a specific device from the DynamoDB table and
+    // checks if the device is offline.
+    // timeout: aligned with API Gateway max timeout to accommodate Athena cold starts, see history stack.
+    const lambdaHandleLiveData = new PythonFunction(this, 'HandleLiveData', {
       entry: path.join(__dirname, '..', 'src', 'lambda', 'live'),
       index: 'handle_live_data.py',
       handler: 'handler',
       runtime: lambda.Runtime.PYTHON_3_12,
-      timeout: cdk.Duration.seconds(15),
+      timeout: cdk.Duration.seconds(29),
       environment: {
-        TABLE_NAME: liveTable.tableName  
+        TABLE_NAME: liveTable.tableName
       }
     });
 
     liveTable.grantReadData(lambdaHandleLiveData);
-
   }
 }
